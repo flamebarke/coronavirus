3110e51f6ed7a473854bf0320b164eacdeed1ca4
Murali Ravipudi <murali.ravipudi@gmail.com>
2020-03-29 20:19:17 +0530

    Reorganize code
    Moved the code into a couple of functions
    coronavirus can be imported as a module now

--- coronavirus.py
+++ coronavirus.py
@@ -4,22 +4,24 @@
 
 from __future__ import absolute_import, division
 
-from   bs4                      import BeautifulSoup
 import datetime
+import sys
+
+from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import sys
-from   tabulate                 import tabulate
+from tabulate import tabulate
 
-url = 'https://www.worldometers.info/coronavirus/'
+URL = 'https://www.worldometers.info/coronavirus/'
+
 
 class HTMLTableParser:
 
     def parse_url(self, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
-        return [(table['id'],self.parse_html_table(table))\
-            for table in soup.find_all('table')]
+        return [(table['id'], self.parse_html_table(table))
+                for table in soup.find_all('table')]
 
     def parse_html_table(self, table):
         n_columns = 0
@@ -32,24 +34,25 @@ class HTMLTableParser:
                 n_rows += 1
                 if n_columns == 0:
                     n_columns = len(td_tags)
-        
+
         th_tags = row.find_all('th')
         if len(th_tags) > 0 and len(column_names) == 0:
             for th in th_tags:
                 column_names.append(th.get_text())
-        
+
         if len(column_names) > 0 and len(column_names) != n_columns:
             raise Exception("Column titles do not match the number of columns")
 
-        columns = column_names if len(column_names) > 0 else range(0,n_columns)
-        df = pd.DataFrame(columns = columns,
-                    index = range(0,n_rows))
+        columns = column_names if len(
+            column_names) > 0 else range(0, n_columns)
+        df = pd.DataFrame(columns=columns,
+                          index=range(0, n_rows))
         row_marker = 0
         for row in table.find_all('tr'):
             column_marker = 0
             columns = row.find_all('td')
             for column in columns:
-                df.iat[row_marker,column_marker] = column.get_text()
+                df.iat[row_marker, column_marker] = column.get_text()
                 column_marker += 1
             if len(columns) > 0:
                 row_marker += 1
@@ -59,19 +62,37 @@ class HTMLTableParser:
                 df[col] = df[col].astype(float)
             except ValueError:
                 pass
-        
+        df.columns = ["Country", "Cases", "+", "Deaths",
+                      "+", "Recovered", "Active", "Critical",
+                      "CPM", "DPM"]
         return df
-    
-time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M%Z")
-print("\n" + "Date/Time >: " + time)
-print("Counters are reset at 23:59UTC" + "\n")
 
-hp = HTMLTableParser()
-table = hp.parse_url(url)[0][1]
-print(tabulate(table, headers=["#","Country","Cases","+","Deaths","+",\
-"Recovered","Active","Critical","CPM","DPM","Tests","TPM"], tablefmt='psql'))
 
-if len(sys.argv) == 2:
-    table.to_csv(time + '.csv')
-else:
-    exit
+def get_worldometer_stats():
+    """
+    Returns a pandas DataFrame with worldometers coronovirus stats.
+    """
+
+    hp = HTMLTableParser()
+    table = hp.parse_url(URL)[0][1]
+    return table
+
+
+def display_stats(export=False):
+
+    time = datetime.datetime.now(
+        datetime.timezone.utc).strftime("%Y-%m-%d %H:%M%Z")
+    print("\n" + "Date/Time >: " + time)
+    print("Counters are reset at 23:59UTC" + "\n")
+
+    table = get_worldometer_stats()
+
+    print(tabulate(table, headers=["#"] + list(table.columns),
+                   tablefmt='psql'))
+
+    if export:
+        table.to_csv(time + '.csv')
+
+
+if __name__ == "__main__":
+    display_stats(len(sys.argv) == 2)
